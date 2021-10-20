f76ba0c942b70d83ba5e3eda236025b079d79ab4
samantha202 <nsamanthaduclaire@gmail.com>
2021-10-20 11:07:48 +0200

    dynamic cart

---
+++ src/app/service/food-list.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {Food} from 'src/app/models/food';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FoodListService {
+  food! : Food[];
+  f!: Food;
+  constructor() 
+  {
+    this.food = new Array();
+  }
+  public add(name:string,categories:string,montant:number)
+  {
+    this.f = new Food(name,categories,montant);
+    this.food.push(this.f);
+  }
+}
--- src/app/views/website/website.component.ts
+++ src/app/views/website/website.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FoodListService} from 'src/app/service/food-list.service';
+import {Food} from 'src/app/models/food'
 @Component({
   selector: 'app-website',
   templateUrl: './website.component.html',
@@ -7,11 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WebsiteComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  food!: Food[];
+  f!: Food;
+  count:number;
+  constructor(private foodList:FoodListService) { 
+    this.food = new Array();
+    //this variable will be incremented each time an item is added to the cart
+    this.count = 0;
   }
+  ngOnInit() {
+  }
+   //this function allows us to add each article in the article list
   addList(nom:string,categorie:string,montant:number)
   {
+    this.foodList.add(nom,categorie,montant);
+    this.count++;
   }
 }
